465fc2bf7d7794b237f7e1320cb8541c8af934cf
pain-arch <himulol123@gmail.com>
2026-01-07 22:28:08 +0600

    feat: add Header component and integrate it into RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +36,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider dynamic>
-          <ConvexClientProvider>{children}</ConvexClientProvider>
+          <ConvexClientProvider>
+            <Header/>
+            {children}
+            
+          </ConvexClientProvider>
         </ClerkProvider>
       </body>
     </html>
